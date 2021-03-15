d85f521e1de5f4f90a081c4955c142ad339ce71a
Andrei <a.demyanof@yandex.ru>
2021-03-15 21:00:38 +0300

    Прикрутили представление статистики

--- Lesson_1/ViewModels/MainWindowViewModel.cs
+++ Lesson_1/ViewModels/MainWindowViewModel.cs
@@ -4,10 +4,12 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Lesson_1.Infrastructure;
 using Lesson_1.Models;
 using Lesson_1.ViewModels;
 using MailSender.lib.ViewModels.Base;
+using MailSender.lib.Commands;
 
 namespace Lesson_1.ViewModels
 {
@@ -23,6 +25,22 @@ namespace Lesson_1.ViewModels
         public string Status { get => _Status; set => Set(ref _Status, value); }
 
         public ObservableCollection<Server> Servers { get; } = new();
+
+        #region Команды
+
+        private ICommand _LoadServersCommand;
+
+        public ICommand LoadServersCommand => _LoadServersCommand
+            ??= new LambdaCommand(OnLoadServersCommandExecuted, CanLoadServersCommandExecute);
+
+        private bool CanLoadServersCommandExecute(object p) => Servers.Count == 0;
+
+        private void OnLoadServersCommandExecuted(object p)
+        {
+            LoadServers();
+        }
+
+        #endregion
 
         public MainWindowViewModel(ServersRepository Servers) { _Servers = Servers; }
 
